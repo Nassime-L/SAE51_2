333da9af36b85bbf9062b22ee6f753f56a70ff3c
Nassime-L <nassime.lahbib@univ-rouen.fr>
2024-03-12 16:13:12 +0100

    récupération IP mariadb

--- script/del_users.sh
+++ script/del_users.sh
@@ -1,4 +1,6 @@
+### IP du conteneur MariaDB ###
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb) 
 ### Script permettant de supprimer TOUT les utilisateurs sauf l'ADMIN ###
-mysql -u root -p'foo' -h localhost < ~/SAE51_2/data/del_users.sql
+mysql -u root -p'foo' -h @host < ~/SAE51_2/data/del_users.sql
 echo "Tout les utilisateurs ont été supprimés"
 #TRUNCATE permet de réinitialiser l'auto-incrément
