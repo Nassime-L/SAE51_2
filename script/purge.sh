2cd13de0dfcfdcf413e8ddeefb47337237071cd9
Nassime-L <nassime.lahbib@univ-rouen.fr>
2024-03-12 16:18:07 +0100

    màj

--- script/purge.sh
+++ script/purge.sh
@@ -1,5 +1,5 @@
 ### Script permettant de supprimer tout les conteneurs et les volumes associés ###
 echo "Supression de conteneur, des images et de volumes associés..."
-docker container rm -f -v upshift/dolibarr:latest adminer:latest mariadb:latest
+docker container rm -f -v dolibarr adminer mariadb
 docker rmi -f adminer upshift/dolibarr mariadb
 
