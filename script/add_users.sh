123a1cc873ac86ddc30adc031cf94c96be04281d
Nassime-L <nassime.lahbib@univ-rouen.fr>
2024-03-12 16:13:02 +0100

    récupération IP mariadb

--- script/add_users.sh
+++ script/add_users.sh
@@ -1,3 +1,5 @@
+### IP du conteneur MariaDB ###
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb) 
 ### Ajout des utilisateurs sans passer par dolibarr ###
-mysql -u root -p'foo' -h localhost < ~/SAE51_2/data/users.sql
+mysql -u root -p'foo' -h @host < ~/SAE51_2/data/users.sql
 echo "Utilisateurs ajoutés !"
