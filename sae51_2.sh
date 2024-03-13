4e403360b6051df7c7aef42f41d9aa43f7c1494b
Nassime-L <nassime.lahbib@univ-rouen.fr>
2024-03-13 14:34:54 +0100

    ajout d'une attente de 30 sec

--- sae51_2.sh
+++ sae51_2.sh
@@ -1,12 +1,13 @@
 ### Script final   ###
 
 ./run.sh
-
+echo "démarrage des conteneurs..."
+sleep 30
 ### création des utilisateurs ###
 
 script/./add_users.sh
 
-### Script de sauvegarde BDD ###
+### sauvegarde BDD ###
 
 crontab -u $USER crontab
 echo "La BDD dolibarr sera sauvegardé tout les dimanches à 22h !"
